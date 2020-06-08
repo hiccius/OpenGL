5bec8f52d6a721953f9d64c5695b77bbb56f92e9
hiccius <alex.bartolome@gmail.com>
2020-06-08 18:48:51 +0100

    Add example of lighting maps

---
+++ 04-lighting/examples/07-lighting_maps.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <array>
+
+#include "helpers.hpp"
+#include "gladhelpers.hpp"
+#include "window.ipp"
+#include "shaderprogram.hpp"
+#include "vertexdatahandler.ipp"
+#include "texture.hpp"
+#include "matrixderived.hpp"
+#include "camera.hpp"
+#include "vertexbufferobject.ipp"
+
+
+int main()
+{
+    constexpr int screenHeight{800};
+    constexpr int screenWidth{600};
+    constexpr double aspect{static_cast<double>(screenHeight) / screenWidth};
+
+    try
+    {
+        // Init window
+        CWindow window;
+        CCamera camera{0.1, 2.5, aspect};
+        window.SetUp(screenHeight, screenWidth, "LearnOpenGL", true, &camera);
+        window.SetMouseControl();
+
+        // Shader programs
+        auto objectShaderProgram{CShaderProgram::Build("07-objectshader.vert", "07-objectshader.frag")};
+        auto lightShaderProgram{CShaderProgram::Build("lightshader.vert", "lightshader.frag")};
+
+        objectShaderProgram.Use();
+        objectShaderProgram.SetUniform("model", CMatrix{});
+
+        CTexture diffuseMap;
+        diffuseMap.GenerateTexture("container2.png");
+        diffuseMap.SetTextureUnitIndex(0);
+        objectShaderProgram.SetUniform("material.diffuse",      0);
+
+        CTexture specularMap;
+        specularMap.GenerateTexture("container2_specular.png");
+        specularMap.SetTextureUnitIndex(1);
+        objectShaderProgram.SetUniform("material.specular",     1);
+
+        objectShaderProgram.SetUniform("material.shininess",    64.0f);
+
+        constexpr std::array<float, 3> lightPosition{1.2f, 1.0f, 2.0f};
+        objectShaderProgram.SetUniform("light.position",    lightPosition);
+        objectShaderProgram.SetUniform("light.ambient",     0.2f, 0.2f, 0.2f);
+        objectShaderProgram.SetUniform("light.diffuse",     0.5f, 0.5f, 0.5f);
+        objectShaderProgram.SetUniform("light.specular",    1.0f, 1.0f, 1.0f);
+
+        lightShaderProgram.Use();
+        lightShaderProgram.SetUniform("model", CMatrix{}.Translate(lightPosition).Scale(0.2f));
+
+        // Vertex data
+        constexpr float vertices[] =
+        {
+             // positions         // normals           // texture coords
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+        };
+        CVertexBufferObject vbo;
+        vbo.AddVertexData(vertices);
+
+        CVertexDataHandler objectData;
+        objectData.AddVertexBufferObject(&vbo);
+        objectData.AddAttribute(3, 8, 0);
+        objectData.AddAttribute(3, 8, 3);
+        objectData.AddAttribute(2, 8, 6);
+
+        CVertexDataHandler lightData;
+        lightData.AddVertexBufferObject(&vbo);
+        lightData.AddAttribute(3, 8, 0);
+
+        // Render loop
+        while (window.IsOpen())
+        {
+            // Poll keys
+            window.UpdateDeltaFrames();
+            window.PollCloseKey(Key::Escape);
+            window.PollMovementKeys(Key::W, Key::A, Key::S, Key::D);
+
+            // Render
+            window.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            diffuseMap.ActivateAndBind();
+            specularMap.ActivateAndBind();
+
+            objectShaderProgram.Use();
+            objectShaderProgram.SetUniform("projection",    static_cast<const CMatrix&>(camera.GetPerspectiveMatrix()));
+            objectShaderProgram.SetUniform("view",          static_cast<const CMatrix&>(camera.GetViewMatrix()));
+            objectShaderProgram.SetUniform("viewPosition",  camera.GetPositionVector());
+            objectData.DrawArrays(36);
+
+            lightShaderProgram.Use();
+            lightShaderProgram.SetUniform("projection", static_cast<const CMatrix&>(camera.GetPerspectiveMatrix()));
+            lightShaderProgram.SetUniform("view",       static_cast<const CMatrix&>(camera.GetViewMatrix()));
+            lightData.DrawArrays(36);
+
+            // Poll events and redraw window
+            window.RedrawAndPoll();
+        }
+
+        return 0;
+    }
+    catch (const OpenGLException& exc)
+    {
+        std::cerr << exc.what() << std::endl;
+        return -1;
+    }
+}
